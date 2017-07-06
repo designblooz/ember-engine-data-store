546f7e3783c73eb222dc7fa2f60abb46777771a2
Paul Kim <paulkim@designblooz.com>
2017-07-06 18:37:56 -0400

    fix for recent ember data changes

--- addon/data-adapters/debug-adapter.js
+++ addon/data-adapters/debug-adapter.js
@@ -1,10 +1,17 @@
 import Ember from 'ember';
-import DebugAdapter from 'ember-data/-private/system/debug/debug-adapter';
-import { assert, warn } from "ember-data/-private/debug";
+import DS from 'ember-data';
+import { assert, warn } from "ember-data/-debug/index";
 
 const { inject, getOwner, isBlank, isPresent } = Ember;
 
-export default DebugAdapter.extend({
+function getPod(type, key, prefix) {
+  var match = key.match(new RegExp('^/?' + prefix + '/(.+)/' + type + '$'));
+  if (match) {
+    return match[1];
+  }
+}
+
+export default DS.DebugAdapter.extend({
   engineStore: inject.service('engine-store'),
   getRecords(modelClass, modelName = '') {
     let records;
@@ -19,37 +26,107 @@ export default DebugAdapter.extend({
     return records;
   },
   getModelTypes() {
-    let containerDebugAdapter = this.get('containerDebugAdapter');
-    let types;
-
-    if (containerDebugAdapter.canCatalogEntriesByType('model')) {
-      types = containerDebugAdapter.catalogEntriesByType('model');
-    } else {
-      types = this._getObjectsOnNamespaces();
-    }
+    const containerDebugAdapter = this.get('containerDebugAdapter');
+    let types = this.getModelEntries();
 
     // New adapters return strings instead of classes.
-    types = Ember.A(types).map((name) => {
-      return this._getKlassName(name);
+    types = Ember.A(types).map((type) => {
+      return this._getKlassName(type);
     });
 
     types = Ember.A(types).filter(type => this.detect(type.klass));
 
     return Ember.A(types);
   },
-  _getKlassName(name) {
-    let modelClass = this._nameToClass(name);
-    let appName = getOwner(this).application.modulePrefix;
 
-    if(!isBlank(modelClass) && !modelClass.toString().includes(appName)) {
-      name = `${modelClass.toString().split('@').shift()}#${name}`;
+  getModelEntries() {
+    const containerDebugAdapter = this.get('containerDebugAdapter');
+    const moduleNames = containerDebugAdapter._moduleRegistry.moduleNames();
+    const types = Ember.A();
+    const type = 'model';
+
+    const prefix = containerDebugAdapter.namespace.modulePrefix;
+
+    for (let i = 0, l = moduleNames.length; i < l; i++) {
+      const key = moduleNames[i];
+
+      if (key.includes(type) && this._isAppModel(key)) {
+        let name = getPod(type, key, containerDebugAdapter.namespace.podModulePrefix || prefix);
+        if (!name) {
+          const splitKey = key.split('/');
+          name = {
+            modulePrefix: splitKey.shift(),
+            name: key.split('models/').pop()
+          }
+        }
+        types.addObject(name);
+      }
+    }
+    return types;
+  },
+
+  _isAppModel(moduleName) {
+    const appPrefixes = this._getAppPrefixes();
+    const isAppModelName = appPrefixes.find(prefix => moduleName.includes(prefix));
+    const isAppModel = isAppModelName ? moduleName.split('models/').length > 1 : false;
+    const isMirage = moduleName.includes('mirage');
+
+    return isAppModel && !isMirage;
+  },
+
+  _getAppPrefixes() {
+    const containerDebugAdapter = this.get('containerDebugAdapter');
+    const namespaces = Ember.A();
+
+    namespaces.push(containerDebugAdapter.namespace.modulePrefix);
+    Object.keys(containerDebugAdapter.namespace.engines).forEach(engine =>
+      namespaces.push(Ember.String.dasherize(engine))
+    );
+
+    return namespaces;
+  },
+
+  _getKlassName(type) {
+    const modelClass = this._nameToKlass(type);
+    const appName = getOwner(this).application.modulePrefix;
+    let name = type.name;
+
+    if (appName !== type.modulePrefix) {
+      name = `${type.modulePrefix}#${type.name}`;
     }
 
-    return {
-      klass: modelClass,
-      name: name
+    if (modelClass) {
+      return {
+        klass: modelClass,
+        name
+      }
     }
   },
+
+  _nameToKlass({ modulePrefix, name }) {
+    let model, engineName, engineStore;
+
+    let appName = getOwner(this).application.modulePrefix;
+
+    if (modulePrefix !== appName) {
+      engineStore = this.get('engineStore').storeFor(modulePrefix);
+    }
+
+    if (typeof name === 'string') {
+      model = getOwner(this).factoryFor(`model:${name}`);
+
+      if(!model && engineStore) {
+        model = engineStore.modelFactoryFor(name);
+      } else if(!model) {
+        model = this.get('engineStore').lookupFactory(`model:${name}`);
+      }
+    }
+
+    if (model) {
+      return model.class;
+    }
+  },
+
   _nameToClass(type) {
     let model, engineName, engineStore;
     let typeSplit = type.split('#');
@@ -63,14 +140,14 @@ export default DebugAdapter.extend({
     if (typeof type === 'string') {
       model = getOwner(this).factoryFor(`model:${type}`);
 
-      if(isBlank(model) && engineStore) {
+      if(!model && engineStore) {
         model = engineStore.modelFactoryFor(modelName);
       }
-      else if(isBlank(model)) {
+      else if(!model) {
         model = this.get('engineStore').lookupFactory(`model:${type}`);
       }
     }
 
-    return model;
+    return model.class;
   }
 });
